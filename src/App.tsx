f36162bf34686cdeb0060ccd68164b75f59ea332
Flavia <250687566+flaviaonorato00-lab@users.noreply.github.com>
2026-09-23 09:56:31 +0200

    Add guess submission logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,47 @@
+import { useState, type FormEvent } from 'react'
 import './App.css'
 import GameRow from './GameRow'
 
 
 function App() {
 
+  const [currentGuess, setCurrentGuess] = useState<string>("");
+  const [guesses, setGuesses] = useState<string[]>([]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (currentGuess.length !== 5) {
+      return
+    }
+
+    setGuesses([...guesses, currentGuess])
+    setCurrentGuess("")
+  }
 
   return (
-      <main className="app">
-        <h1>WORDLE</h1>
+    <main className="app">
+      <h1>WORDLE</h1>
 
-        <div className="game-board">
-          {Array.from({ length: 6 }).map((_, index) => (
-            <GameRow key={index} />
-          ))}
-        </div>
-      </main>
+      <div className="game-board">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <GameRow
+            key={index}
+            word={
+              guesses[index] ?? /* nullish coalescing operator => if the left is "null" or undefined" then use the code on the right*/
+              (index === guesses.length ? currentGuess : "")
+            }
+             />
+        ))}
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          value={currentGuess}
+          maxLength={5}
+          onChange={(e) => setCurrentGuess(e.target.value.toLocaleUpperCase())} />
+      </form>
+    </main>
   )
 }
 
--- src/GameRow.tsx
+++ src/GameRow.tsx
@@ -1,9 +1,15 @@
-function GameRow() {
+type GameRowProps = {
+    word: string
+}
+
+function GameRow({ word }: GameRowProps) {
 
     return(
         <div className="game-row">
             {Array.from({ length: 5 }).map((_, index) => (
-                <div className="letter-box" key={index}></div>
+                <div className="letter-box" key={index}>
+                    {word[index]}
+                </div>
             ))}
         </div>
     )
